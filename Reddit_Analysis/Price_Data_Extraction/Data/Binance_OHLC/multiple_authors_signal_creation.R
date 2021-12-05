aca0e2ba82f1a11965cace35c97e1b53a27d84c2
Kai Cooper <kai.cooper19@imperial.ac.uk>
2021-12-05 15:10:45 +0100

    Created signals for other authors with more data over a longer interval.

--- Reddit_Analysis/Price_Data_Extraction/Data/Binance_OHLC/multiple_authors_signal_creation.R
+++ Reddit_Analysis/Price_Data_Extraction/Data/Binance_OHLC/multiple_authors_signal_creation.R
@@ -13,25 +13,42 @@ for (i in 1:length(filenames)){
 }
 names(post_times) <- names
 
-source("add_signal.R")
-source("create_signal.R")
-source("impute.R")
-
-btc_authors <- c("simplelifestyle", "CoinCorner_Sam")
+btc_authors <- c("simplelifestyle", "CoinCorner") #actually CoinCorner_Sam
 
 ETHUSD1m <- as.data.frame(read.csv("ETHUSDT-1m-binance.csv"))
 BTCUSD1m <- as.data.frame(read.csv("BTCUSDT-1m-binance.csv"))
 
+source("impute.R")
+
 ETHUSD1m.imputed <- impute(prices=ETHUSD1m, gap="min")
 BTCUSD1m.imputed <- impute(prices=BTCUSD1m, gap="min")
 
+source("create_signal.R")
+
 signals <- list()
 for (i in 1:length(filenames)){
-  if(names(post_times)[i] %in% btc_authors){
-    signals[[i]] <- create_signal(post_times[[i]], BTCUSD1m, 'min')
+  if(names(post_times)[i] %in% btc_authors){ #use bitcoin data for bitcoin authors 
+    signals[[i]] <- create_signal(post_times[[i]], BTCUSD1m.imputed, 'min')
   }else{
-    signals[[i]] <- create_signal(post_times[[i]], ETHUSD1m, 'min')
+    signals[[i]] <- create_signal(post_times[[i]], ETHUSD1m.imputed, 'min')
   }
 }
 names(signals) <- names
 
+#initialise
+old_btc <- BTCUSD1m.imputed
+old_eth <- ETHUSD1m.imputed
+
+source("add_signal.R")
+
+for(i in which(names(signals) %in% btc_authors)){
+    new_btc <- add_signal(old_btc, signals, i) 
+    old_btc <- new_btc
+}
+for(i in which(!names(signals) %in% btc_authors)){
+    new_eth <- add_signal(old_eth, signals, i)
+    old_eth <- new_eth
+}
+
+BTC_imp_w_signal <- new_btc
+ETH_imp_w_signal <- new_eth
