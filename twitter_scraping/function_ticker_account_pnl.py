9386cfb3e0b748d742621680a31ef4ca3b52cf70
yacinetrad <91531909+yacinetrad@users.noreply.github.com>
2021-12-08 12:49:46 +0100

    Update function_ticker_account_pnl.py
    added comparison with results of sentiment analysis

--- twitter_scraping/function_ticker_account_pnl.py
+++ twitter_scraping/function_ticker_account_pnl.py
@@ -16,11 +16,16 @@ def buy_when_tweet(tickers, user, k, df):
     tweets_df2 = pd.DataFrame(tweets_list2, columns=['timestamp', 'Tweet Id', 'Text', 'Username'])
 
     df['buy_signal'] = 0
+    df['buy_signal_sentiment'] = 0
     for index_1, row in tweets_df2.iterrows():
         for index, element in df.iterrows():
 
             if str(row['timestamp'])[0:14] == str(element['timestamp'])[0:14]:
                 df.at[index, 'buy_signal'] = 1
+                text = row['Text']
+                blob = TextBlob(text)
+                if blob.sentiment.polarity >= 0:
+                    df.at[index, 'buy_signal_sentiment'] = 1
 
     pnl = list()
     pnl.append(1000)
@@ -34,6 +39,21 @@ def buy_when_tweet(tickers, user, k, df):
     plt.xlabel('Entry points')
     plt.ylabel('Portfolio value')
     plt.title(tickers + ' strategy profit and loss from' + user)
+    plt.show()
+    return
+
+    pnl_1 = list()
+    pnl_1.append(1000)
+
+    for index, row in df.iterrows():
+        if row['buy_signal_sentiment'] == 1:
+            new_profit = pnl_1[-1] * (df.at[index + k, 'close'] / df.at[index, 'close'])
+            pnl_1.append(new_profit)
+
+    plt.plot(pnl_1)
+    plt.xlabel('Entry points')
+    plt.ylabel('Portfolio value')
+    plt.title(tickers + ' Strategy profit and loss from sentiment analysis of ' + user+' tweets')
     plt.show()
     return
 
